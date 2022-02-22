71e4fbdec8117fe9eed4ca90154929d4306cadd6
Julia-Tuschenko <84866878+Julia-Tuschenko@users.noreply.github.com>
2022-02-22 21:17:22 +0000

    Deploying to gh-pages from @ Julia-Tuschenko/goit-react-hw-05-movies@fe7795a6483b73dccfef351211ebf42cb007a582 🚀

---
+++ static/js/704.a27bd823.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[704],{2704:function(t,i,e){e.r(i);var r=e(184);i.default=function(t){var i=t.movie;return(0,r.jsxs)(r.Fragment,{children:[(0,r.jsx)("div",{children:(0,r.jsx)("img",{src:i.backdrop_path?"".concat("https://image.tmdb.org/t/p/original").concat(i.backdrop_path):"\ud83e\udd14",alt:i.title})}),(0,r.jsx)("div",{children:(0,r.jsx)("h2",{children:i.original_title})})]})}}}]);
+//# sourceMappingURL=704.a27bd823.chunk.js.map
